8c349c9d1a07e7f580eac26302aef6687ebfc0d3
Yogesh Sharma <yogeshnawhal@gmail.com>
2020-04-07 15:58:00 +0000

    bug

---
+++ about.blade.php
@@ -0,0 +1,381 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>shoelacestech</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Work+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/animate.css">    
+    
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body>
+  
+ 
+
+  <div class="site-wrap">
+
+
+
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    
+    
+    <div class="site-navbar-wrap js-site-navbar bg-white">
+      
+      <div class="container">
+        <div class="site-navbar bg-light">
+          <div class="row align-items-center">
+            <div class="col-2 col-xl-2">
+              <h1 class="mb-3"><a href="/welcome" class="text-black h2 mb-3"><img src="images/logo.png" width="52" height="42"></a></h1>
+            </div>
+            <div class="col-10">
+              <nav class="site-navigation text-right" role="navigation">
+                <div class="container">
+                  <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+                 <ul class="site-menu js-clone-nav d-none d-lg-block">
+                    <li><a href="/welcome">Home</a></li>
+                    <li class="has-children active">
+                      <a  href="/about">About</a>
+                      <ul class="dropdown arrow-top">
+                        <li><a href="/basic_profile">Basic Profile</a></li>
+                        <li><a href="/key_man">Key Man</a></li>
+                        <!-- <li><a href="faq.html">FAQ</a></li> -->
+                        <!-- <li><a href="why-choose-us.html">Salient Features</a></li> -->
+                       <!--  <li class="has-children">
+                          <a href="#">Sub Menus</a> -->
+                         <!--  <ul class="dropdown">
+                            <li><a href="testimonials.html">Testimonials</a></li>
+                            <li><a href="gallery.html">Gallery</a></li>
+                            <li><a href="why-choose-us.html">Why Choose Us</a></li>
+                          </ul> -->
+                        <!-- </li> -->
+                      </ul>
+                    </li>
+                    <li class="has-children">
+                      <a href="/footwear_industry">Footwear Accessories</a>
+                       <ul class="dropdown arrow-top">
+                        <li><a href="shoe_laces">Shoe Laces</a></li>
+                        <li><a href="tapes">Tapes</a></li>
+                        <li><a href="">Cords</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="/blog">Garment Accessories</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                   
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    <div class="slide-one-item home-slider owl-carousel">
+       <div class="site-blocks-cover inner-page overlay xyz" style="background-image: url(images/theme.jpg);" data-aos="fade" data-stellar-background-ratio="0.5" style="width: 100%;">
+         <div class="row align-items-center justify-content-center">
+            <div class="col-md-6 text-center" data-aos="fade">
+              <h1 class="font-secondary  font-weight-bold text-uppercase">About us</h1>
+               <div><a href="/welcome">Home</a> <span class="mx-2 text-white">&bullet;</span> <span class="text-white">About us</span></div>
+            </div>
+          </div>
+      </div>
+   </div>
+
+      <div class="site-section">
+      <div class="container"> 
+      <section class="section-gap" id="service">
+            <div class="container">
+              <div class="row d-flex justify-content-center">
+                <div class="col-md-8 pb-40 header-text">
+                  <h1>Why choose us</h1>
+                  <p>
+                    Who are in extremely love with eco friendly system.
+                  </p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-4 col-md-6">
+                  <div class="single-service">
+                    <h4><span class="lnr lnr-user"></span>Modern Infrastructure</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div class="single-service">
+                    <h4><span class="lnr lnr-license"></span>Trained Workforce</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>                
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                  <div class="single-service">
+                    <h4><span class="lnr lnr-phone"></span>Continuous R&D</h4>
+                    <p>
+                      Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+                    </p>                
+                  </div>
+                </div>         
+              </div>
+            </div>  
+          </section>
+  
+        <section class="service-area section-gap" id="service">
+            <div class="container">
+              <div class="site-half block">
+                <div class="img-bg-1 right" style="background-image: url('images/2001.jpeg');" data-aos="fade"></div>
+                <div class="container">
+                  <div class="row no-gutters align-items-stretch">
+                    <div class="col-lg-5 mr-lg-auto py-5" >
+                      <span class="caption d-block mb-2 font-secondary font-weight-bold"></span>
+                      <h2 class="site-section-heading text-uppercase font-secondary mb-5">Our Mission</h2>
+                      <p style="text-align: justify;"> Our misson is winning the confidence of all our customers. Our specialisation is to manufacture and supply as per customer needs and specifications, Simultaneously meeting all international quality standards.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <br>
+               <div class="site-half">
+                  <div class="img-bg-1" style="background-image: url('images/history.png');" data-aos="fade"></div>
+                  <div class="container">
+                    <div class="row no-gutters align-items-stretch">
+                      <div class="col-lg-5 ml-lg-auto py-5">
+                        <span class="caption d-block mb-2 font-secondary font-weight-bold"> </span>
+                        <h2 class="site-section-heading text-uppercase font-secondary mb-5">Company History</h2>
+                        <p style="text-align: justify;"> The company began its operations in 1964 by Mr. Baldev chander arora (Founder) . Since then we are supplying our customers to there satisfaction.</p>
+                         <p><a href="/basic_profile" class="btn btn-primary text-white px-4">Read More</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>  
+          </section>
+
+        <section class="feature-area">
+          <div class="container-fluid">
+            <div class="row justify-content-center align-items-center">
+              <div class="col-lg-3 feat-img no-padding">
+                <img class="img-fluid" src="images/007.jpg" alt="">
+              </div>
+              <div class="col-lg-3 no-padding feat-txt">
+                <h1>Who we are</h1>
+                <p>
+                  Computer users and programmers have become so accustomed to using Windows, even for the changing capabilities and the appearances of the graphical.
+                </p>
+              </div>
+              <div class="col-lg-3 feat-img no-padding">
+                <img class="img-fluid" src="images/002.jpg" alt="">             
+              </div>
+              <div class="col-lg-3 no-padding feat-txt">
+                <h6 class="text-uppercase text-white">Basic & Common Repairs</h6>
+                <h1>What we do</h1>
+                <p >
+                  We are manufactures,traders,suppliers and exporters of shoe laces,tapes,cords and other garment accessories.
+                </p>
+              </div>
+            </div>
+          </div>  
+        </section>
+
+
+      <section class="team-area section-gap" id="team">
+        <div class="container">
+          <div class="row d-flex justify-content-center">
+            <div class="menu-content pb-70 col-lg-8">
+              <div class="title text-center">
+                <h1 class="mb-10">Experienced Mentor Team</h1>
+                <p>Who are in extremely love with eco friendly system.</p>
+              </div>
+            </div>
+          </div>            
+          <div class="row justify-content-center d-flex align-items-center">
+            <div class="col-md-3 single-team">
+                <div>
+                    <img class="img-fluid" src="images/person_1.jpg" alt="">
+                    <div class="align-items-center justify-content-center d-flex">
+                  <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+                    </div>
+                </div>
+                <div class="meta-text mt-30 text-center">
+                  <h4>Ethel Davis</h4>
+                  <p>Managing Director (Sales)</p>                        
+                </div>
+            </div>
+            <div class="col-md-3 single-team">
+                <div class="">
+                    <img class="img-fluid" src="images/person_2.jpg" alt="">
+                    <div class="align-items-center justify-content-center d-flex">
+                  <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+                    </div>
+                </div>
+                <div class="meta-text mt-30 text-center">
+                  <h4>Rodney Cooper</h4>
+                  <p>Creative Art Director (Project)</p>            
+                </div>
+            </div>  
+            <div class="col-md-3 single-team">
+                <div class="">
+                    <img class="img-fluid" src="images/person_3.jpg" alt="">
+                    <div class="align-items-center justify-content-center d-flex">
+                 <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+                    </div>
+                </div>
+                <div class="meta-text mt-30 text-center">
+                  <h4>Dora Walker</h4>
+                  <p>Senior Core Developer</p>            
+                </div>
+            </div>  
+            <div class="col-md-3 single-team">
+                <div class="">
+                    <img class="img-fluid" src="images/person_4.jpg" alt="">
+                    <div class="align-items-center justify-content-center d-flex">
+                 <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                  <a href="#" class="p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+                    </div>
+                </div>
+                <div class="meta-text mt-30 text-center">
+                  <h4>Lena Keller</h4>
+                  <p>Creative Content Developer</p>           
+                </div>
+            </div>                                                  
+        
+          </div>
+        </div>  
+      </section>
+      </div>
+    </div>
+
+    <div class="py-5 bg-primary">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-6 text-center mb-3 mb-md-0">
+            <h2 class="text-uppercase text-white mb-4" data-aos="fade-up">Try For Your Next Project</h2>
+            <a href="/contact" class="btn btn-bg-primary font-secondary text-uppercase" data-aos="fade-up" data-aos-delay="100">Contact Us</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+     <div class="">
+      <a title="Send WhatsApp" href="https://api.whatsapp.com/send?phone= +91 9999667721&amp;text=Hi, Team" target=”_blank”><i style="background-color: #25D366; color: white; background:linear-gradient(#25D366,#25D366)10px 84%/0px 9px no-repeat,radial-gradient(circle at center, #25D366 63%,transparent 0);  position: fixed;
+    bottom:1%;right: 5px; font-size: 40px; " class="fab fa-whatsapp "></i></a>
+    </div>
+
+   <footer class="site-footer bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 mb-4 mb-md-0">
+            <h3 class="footer-heading mb-4 text-white">About</h3>
+            <p>We are manufactures,traders,suppliers and exporters of shoe laces,tapes,cords and other garment accessories.</p>
+            <p><a href="/about" class="btn btn-primary text-white px-4">Read More</a></p>
+          </div>
+          <div class="col-md-5 mb-4 mb-md-0 ml-auto">
+            <div class="row mb-4">
+              <div class="col-md-6">
+                <h3 class="footer-heading mb-4 text-white">Quick Menu</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="/welcome">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <!-- <li><a href="/blog">Blog</a></li> -->
+                    <li><a href="/contact">Contacts</a></li>
+                    <li><a href="#">Privacy</a></li>
+                  </ul>
+              </div>
+             <div class="col-md-6">
+                <h3 class="footer-heading mb-4 text-white">Laces</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">Nylon Laces</a></li>
+                    <li><a href="#">Polyester Laces</a></li>
+                    <li><a href="#">Oval Shoe Laces</a></li>
+                    <li><a href="#">Round Shoe Laces</a></li>
+                  </ul>
+              </div>
+            </div>
+
+            <div class="row mb-5">
+              <div class="col-md-12">
+              <h3 class="footer-heading mb-4 text-white">Stay up to date</h3>
+              <form action="#" class="d-flex footer-subscribe">
+                <input type="text" class="form-control rounded-0" placeholder="Enter your email">
+                <input type="submit" class="btn btn-primary rounded-0" value="Subscribe">
+              </form>
+            </div>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="row">
+            <div class="col-md-12"><h3 class="footer-heading mb-4 text-white">Social Icons</h3></div>
+              <div class="col-md-12">
+                <p>
+                  <a href="#" class="pb-2 pr-2 pl-0"><span class="icon-facebook"></span></a>
+                  <a href="#" class="p-2"><span class="icon-twitter"></span></a>
+                  <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+                  <a href="#" class="p-2"><span class="icon-vimeo"></span></a>
+                </p>
+              </div>
+          </div>
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | 
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+
+  
+  </body>
+</html>
