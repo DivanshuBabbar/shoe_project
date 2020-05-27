a2cc87391a7ce6bf55bf4c47df579bb008b38de1
Divanshu <babbardivanshu@gmail.com>
2020-05-27 21:41:14 +0530

    bug

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -5,7 +5,7 @@
 
   <table>
     <tr>
-      <td>This query has been raise by <?php echo $fname  $lname; ?> stating email-id : <?php echo $email; ?> stating message : Interested in Article no : <?php echo $article_no; ?> of usage : <?php echo $usage; ?> in size: <?php echo $size; ?> approz quantity required <?php echo $quantity; ?>  ></td>
+      <td>This query has been raise by <?php echo $fname ;?> <?php echo $lname; ?> stating email-id : <?php echo $email; ?> stating message : Interested in Article no : <?php echo $article_no; ?> of usage : <?php echo $usage; ?> in size: <?php echo $size; ?> approz quantity required <?php echo $quantity; ?>  ></td>
     </tr>
   </table>
 </body>
